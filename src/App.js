f64741b2637c4d8f21c85efaa62a5c02e0f6a874
hitakshi-git <sharmahitakshi343@gmail.com>
2026-04-17 13:02:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000")
+      .then(res => res.json())
+      .then(data => setMessage(data.message))
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Frontend Connected 🚀</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
